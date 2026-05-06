b6182b4997f556076b7a6971a1c61d37346d375c
Prophetjames <mingweishere@gmail.com>
2026-05-06 15:02:53 -0700

    Initial Commit

---
+++ FortuneCookie/FortuneCookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneCookieApp.swift
+//  FortuneCookie
+//
+//  Created by Mingwei Wei on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FortuneCookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
